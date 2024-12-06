450747d7597b7228ffe103a4834d338b50780861
enriquefuenzalidam <enrique.fuenzalida@yahoo.cl>
2024-12-06 17:31:39 -0300

    update

--- next.config.js
+++ next.config.js
@@ -1,25 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     poweredByHeader: false,
     reactStrictMode: true,
-    swcMinify: true,
-    images: { unoptimized: true },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(mp4|webm|ogg|avi)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          publicPath: '/_next/static/videos/',
-          outputPath: 'static/videos/',
-          name: '[name].[ext]',
-        },
+    //    output: 'export',
+//    images: { unoptimized: true },
+webpack(config, options) {
+  // Disable caching temporarily
+  config.cache = false;
+  
+  // Existing file loader rules
+  config.module.rules.push({
+    test: /\.(mp4|webm|ogg|swf|ogv)$/, // Include the file types you need
+    use: {
+      loader: 'file-loader',
+      options: {
+        publicPath: '/_next/static/videos',
+        outputPath: 'static/videos',
+        name: '[name].[hash].[ext]',
+        esModule: false,
       },
-    });
+    },
+  });
 
-    return config;
-  },
+  return config;
+},
   async redirects() {
     return [
       {
